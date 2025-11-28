3f6c55d3bb4b38b09d3081262edfac620d3b5d5c
Davide De Benedittis <davide.debenedittis@gmail.com>
2025-11-28 14:24:11 +0100

    Add multi-robot go-to-goal example notebook

--- src/dimp/utils/__init__.py
+++ src/dimp/utils/__init__.py
@@ -1,1 +1,10 @@
+from dimp.utils.disp_anim import (
+    Animation,              # noqa: F401
+    MultiRobotArtistFlags,  # noqa: F401
+    Player,                 # noqa: F401
+    display_animation,      # noqa: F401
+    plot_distances,         # noqa: F401
+    save_snapshots,         # noqa: F401
+    plot_colour_line,       # noqa: F401
+)
 from dimp.utils.disp import gen_arrow_head_marker, init_matplotlib, get_colors   # noqa: F401
